a38ccb77216d6be7d59fb582a95c8b5daa636b2a
hackerdesu <98327480+hackerdesu@users.noreply.github.com>
2022-02-11 10:16:51 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-curl https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt -o proxies.txt;java -jar MCBOT.jar 120.75.92.217:25565 757 join 360000 -1
+curl https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt -o proxies.txt;java -jar MCBOT.jar 120.75.92.217:25576 757 join 360000 -1
